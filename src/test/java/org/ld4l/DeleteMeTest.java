9a352e367b80086287b164f9dac60294d1fa64d7
Jim Blake <jeb228@cornell.edu>
2016-09-16 14:19:23 -0400

    Restructure for Maven.

---
+++ src/test/java/org/ld4l/DeleteMeTest.java
@@ -0,0 +1,20 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package org.ld4l;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.junit.Test;
+
+/**
+ * TODO
+ */
+public class DeleteMeTest {
+@Test
+public void testMethod() throws ClientProtocolException, IOException {
+	new DeleteMe().method();
+}
+
+
+}
