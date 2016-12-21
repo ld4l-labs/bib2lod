990765839f6fbfe4fa9dfdb98fbb5d8982e0ff27
Rebecca Younes <rebecca.younes@cornell.edu>
2016-12-21 13:51:17 -0500

    Add failing tests to be filled in

--- src/test/java/org/ld4l/bib2lod/configuration/options/JsonUtilsTest.java
+++ src/test/java/org/ld4l/bib2lod/configuration/options/JsonUtilsTest.java
@@ -1,9 +1,61 @@
 package org.ld4l.bib2lod.configuration.options;
 
-public class JsonUtilsTest {
+import static org.junit.Assert.*;
 
-    public JsonUtilsTest() {
-        // TODO Auto-generated constructor stub
+import org.junit.Test;
+import org.ld4l.bib2lod.configuration.options.JsonUtils.InvalidTypeException;
+import org.ld4l.bib2lod.testing.AbstractTestClass;
+
+public class JsonUtilsTest extends AbstractTestClass {
+
+    @Test
+    public void optionalKeyMissing_Succeeds() {
+        fail("optionalKeyMissing_Succeeds not implemented");
+    }
+    
+    @Test
+    public void optionalStringValueNull_Succeeds() {
+        fail("optionalStringValueNull_Succeeds not implemented");
+    }
+    
+    @Test
+    public void optionalStringValueEmpty_Succeeds() {
+        fail("optionalStringValueEmpty_Succeeds not implemented");
+    }
+    
+    @Test
+    public void optionalStringValueInvalidType_ThrowsException() {
+        fail("optionalStringValueInvalidType_ThrowsException not implemented");
+    }
+    
+    @Test
+    public void optionalStringValueValid_Succeeds() {
+        fail("optionalStringValueValid_Succeeds not implemented");
+    }
+    
+    @Test
+    public void requiredKeyMissing_ThrowsException() {
+        fail("requiredKeyMissing_ThrowsException not implemented");
+    }
+    
+    @Test
+    public void requiredStringValueNull_ThrowsException() {
+        fail("requiredStringValueNull_ThrowsException not implemented");
+    }
+    
+    @Test
+    public void requiredStringValueEmpty_ThrowsException() {
+        fail("requiredStringValueEmpty_ThrowsException not implemented");
+    }
+    
+    @Test
+    public void requiredStringValueInvalidType_ThrowsException() {
+        fail("requiredStringValueInvalidType_ThrowsException not implemented");
+    }
+    
+    @Test
+    public void requiredStringValueValid_Succeeds() {
+        fail("requiredStringValueValid_Succeeds not implemented");
     }
 
 }
