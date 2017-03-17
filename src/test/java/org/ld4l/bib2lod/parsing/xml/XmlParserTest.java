5d876b464940172946336010516091ac8e69146c
Rebecca Younes <rebecca.younes@cornell.edu>
2017-03-17 14:26:44 -0400

    Remove unnecessary MockConfiguration class

--- src/test/java/org/ld4l/bib2lod/parsing/xml/XmlParserTest.java
+++ src/test/java/org/ld4l/bib2lod/parsing/xml/XmlParserTest.java
@@ -1,39 +1,24 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
 package org.ld4l.bib2lod.parsing.xml;
 
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.ld4l.bib2lod.configuration.Configuration;
 import org.ld4l.bib2lod.io.InputService.InputDescriptor;
 import org.ld4l.bib2lod.parsing.Parser;
 import org.ld4l.bib2lod.record.Record;
 
-/* $This file is distributed under the terms of the license in /doc/license.txt$ */
-
 import org.ld4l.bib2lod.testing.AbstractTestClass;
-
 
 
 /**
  * Tests class XmlParser.
  */
-/*
- * test plan:
- * - test that an invalid record is ignored
- * - when reading in xml document, skip document or part of document that causes 
- * an error but do not terminate
- * 
- * todo:
- * - remove unused stuff from Configuration
- * - don't use factory? Should it be used, or removed?
- * - add package parsing.xml - move all xml parsers into it
- */
 public class XmlParserTest extends AbstractTestClass {
     
-    private Configuration config;
     private Parser parser;
     
     private static final String OPEN_ROOT_ELEMENT = "<collection>";
@@ -49,10 +34,7 @@ public class XmlParserTest extends AbstractTestClass {
 
     @Before
     public void setUp() {
-        
-        config = new MockConfiguration(null);
-        parser = Parser.instance(config, MockXmlParser.class);
-
+        parser = Parser.instance(null, MockXmlParser.class);
     }        
 
     
