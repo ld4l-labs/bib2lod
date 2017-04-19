33076bed724a0d84fec62e8a4772150d965b9f34
Rebecca Younes <rebecca.younes@cornell.edu>
2017-04-19 10:43:34 -0400

    Update LD4L namespace & URI for new ontology name

--- src/main/java/org/ld4l/bib2lod/ontology/ld4l/Ld4lObjectProp.java
+++ src/main/java/org/ld4l/bib2lod/ontology/ld4l/Ld4lObjectProp.java
@@ -10,13 +10,13 @@ import org.ld4l.bib2lod.ontology.ObjectProp;
  */
 public enum Ld4lObjectProp implements ObjectProp {
 
-    HAS_ACTIVITY(Ld4lNamespace.LD4L, "hasActivity"),
+    HAS_ACTIVITY(Ld4lNamespace.BIBLIOTEKO, "hasActivity"),
     HAS_LANGUAGE(Ld4lNamespace.DCTERMS, "language"),
     HAS_ITEM(Ld4lNamespace.BIBFRAME, "hasItem"),
     HAS_PART(Ld4lNamespace.DCTERMS, "hasPart"),
-    HAS_PREFERRED_TITLE(Ld4lNamespace.LD4L, "hasPreferredTitle"),
+    HAS_PREFERRED_TITLE(Ld4lNamespace.BIBLIOTEKO, "hasPreferredTitle"),
     HAS_TITLE(Ld4lNamespace.BIBFRAME, "title"),
-    IS_AT_LOCATION(Ld4lNamespace.LD4L, "atLocation"),
+    IS_AT_LOCATION(Ld4lNamespace.BIBLIOTEKO, "atLocation"),
     IS_IDENTIFIED_BY(Ld4lNamespace.BIBFRAME, "identifiedBy"),
     IS_INSTANCE_OF(Ld4lNamespace.BIBFRAME, "instanceOf");
 
