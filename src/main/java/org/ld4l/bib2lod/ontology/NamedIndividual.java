e6cf2e85c03ad5609c0f9b3c96112789bd12ec5b
Rebecca Younes <rebecca.younes@cornell.edu>
2017-04-28 09:28:46 -0700

    Add NamedIndividual interface

---
+++ src/main/java/org/ld4l/bib2lod/ontology/NamedIndividual.java
@@ -0,0 +1,10 @@
+package org.ld4l.bib2lod.ontology;
+
+import org.apache.jena.rdf.model.Resource;
+
+public interface NamedIndividual {
+    
+    public String uri(); 
+
+    public Resource resource();
+}
