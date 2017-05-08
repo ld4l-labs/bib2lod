26802689d392a180dbbae7c54503a72034ce9293
David Neiman <david_neiman@harvard.edu>
2017-05-08 16:51:54 -0400

    Add new Type and object property references.

---
+++ src/main/java/org/ld4l/bib2lod/ontology/ld4l/Ld4LocationType.java
@@ -0,0 +1,40 @@
+package org.ld4l.bib2lod.ontology.ld4l;
+
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.ld4l.bib2lod.ontology.Namespace;
+import org.ld4l.bib2lod.ontology.Type;
+
+/**
+ * Enumerates the Location types used in the LD4L BIBFRAME 2 extension and
+ * application profile.
+ */
+public enum Ld4LocationType implements Type {
+        
+    LOCATION(Ld4lNamespace.PROV, "Location");
+    
+    private final String uri;
+    private final Resource ontClass;
+    
+    /**
+     * Constructor
+     */
+    Ld4LocationType(Namespace namespace, String localName) {
+        this.uri = namespace.uri() + localName;
+        this.ontClass = ResourceFactory.createResource(uri);
+    }
+    
+    @Override
+    public String uri() {
+        return uri;
+    }
+
+    @Override
+    public Resource ontClass() {
+        return ontClass;
+    } 
+
+    public static Type superClass() {
+        return LOCATION;
+    }
+}
--- src/main/java/org/ld4l/bib2lod/ontology/ld4l/Ld4lObjectProp.java
+++ src/main/java/org/ld4l/bib2lod/ontology/ld4l/Ld4lObjectProp.java
@@ -11,16 +11,21 @@ import org.ld4l.bib2lod.ontology.ObjectProp;
 public enum Ld4lObjectProp implements ObjectProp {
 
     HAS_ACTIVITY(Ld4lNamespace.BIBLIOTEKO, "hasActivity"),
+    IS_ACTIVITY_OF(Ld4lNamespace.BIBLIOTEKO, "isActivityOf"),
     HAS_LANGUAGE(Ld4lNamespace.DCTERMS, "language"),
     HAS_INSTANCE(Ld4lNamespace.BIBFRAME, "hasInstance"),
     HAS_ITEM(Ld4lNamespace.BIBFRAME, "hasItem"),
     HAS_PART(Ld4lNamespace.DCTERMS, "hasPart"),
     HAS_PREFERRED_TITLE(Ld4lNamespace.BIBLIOTEKO, "hasPreferredTitle"),
     HAS_TITLE(Ld4lNamespace.BIBFRAME, "title"),
+    IS_TITLE_OF(Ld4lNamespace.BIBFRAME, "isTitleOf"),
     IDENTIFIES(Ld4lNamespace.BIBFRAME, "identifies"),
     IS_AT_LOCATION(Ld4lNamespace.BIBLIOTEKO, "atLocation"),
+    IS_LOCATION_OF(Ld4lNamespace.BIBLIOTEKO, "isLocationOf"),
     IS_IDENTIFIED_BY(Ld4lNamespace.BIBFRAME, "identifiedBy"),
-    IS_INSTANCE_OF(Ld4lNamespace.BIBFRAME, "instanceOf");
+    IS_INSTANCE_OF(Ld4lNamespace.BIBFRAME, "instanceOf"),
+    HAS_AGENT(Ld4lNamespace.BIBFRAME, "agent"),
+    IS_AGENT_OF(Ld4lNamespace.BIBLIOTEKO, "isAgentOf");
 
    
     private String uri;
