91750004cb6d457b86745a3d9fd2dbd312cc0920
David Neiman <david_neiman@harvard.edu>
2017-05-11 16:41:25 -0400

    PR changes.

--- src/main/java/org/ld4l/bib2lod/ontology/ld4l/Ld4lObjectProp.java
+++ src/main/java/org/ld4l/bib2lod/ontology/ld4l/Ld4lObjectProp.java
@@ -18,13 +18,13 @@ public enum Ld4lObjectProp implements ObjectProp {
     HAS_PART(Ld4lNamespace.DCTERMS, "hasPart"),
     HAS_PREFERRED_TITLE(Ld4lNamespace.BIBLIOTEKO, "hasPreferredTitle"),
     HAS_TITLE(Ld4lNamespace.BIBFRAME, "title"),
-    IS_TITLE_OF(Ld4lNamespace.BIBFRAME, "isTitleOf"),
+    IS_TITLE_OF(Ld4lNamespace.BIBLIOTEKO, "isTitleOf"),
     IDENTIFIES(Ld4lNamespace.BIBFRAME, "identifies"),
     IS_AT_LOCATION(Ld4lNamespace.BIBLIOTEKO, "atLocation"),
     IS_LOCATION_OF(Ld4lNamespace.BIBLIOTEKO, "isLocationOf"),
     IS_IDENTIFIED_BY(Ld4lNamespace.BIBFRAME, "identifiedBy"),
     IS_INSTANCE_OF(Ld4lNamespace.BIBFRAME, "instanceOf"),
-    HAS_AGENT(Ld4lNamespace.BIBFRAME, "agent"),
+    HAS_AGENT(Ld4lNamespace.BIBLIOTEKO, "hasAgent"),
     IS_AGENT_OF(Ld4lNamespace.BIBLIOTEKO, "isAgentOf");
 
    
