67b068b623a4075d4ec7e2069475885588aec3fc
David Neiman <david_neiman@harvard.edu>
2017-06-07 14:28:14 -0400

    Add data and object properties; add new Type.

---
+++ src/main/java/org/ld4l/bib2lod/ontology/ld4l/Ld4lConceptType.java
@@ -0,0 +1,41 @@
+package org.ld4l.bib2lod.ontology.ld4l;
+
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.ld4l.bib2lod.ontology.Namespace;
+import org.ld4l.bib2lod.ontology.Type;
+
+/**
+ * Enumerates the Concept types used in the LD4L Bibliotek-o extension and
+ * application profile.
+ */
+public enum Ld4lConceptType implements Type {
+        
+    /* List in alpha order */
+    CONCEPT(Ld4lNamespace.SKOS, "Concept");
+    
+    private final String uri;
+    private final Resource ontClass;
+    
+    /**
+     * Constructor
+     */
+    Ld4lConceptType(Namespace namespace, String localName) {
+        this.uri = namespace.uri() + localName;
+        this.ontClass = ResourceFactory.createResource(uri);
+    }
+    
+    @Override
+    public String uri() {
+        return uri;
+    }
+
+    @Override
+    public Resource ontClass() {
+        return ontClass;
+    } 
+
+    public static Type superClass() {
+        return CONCEPT;
+    }
+}
